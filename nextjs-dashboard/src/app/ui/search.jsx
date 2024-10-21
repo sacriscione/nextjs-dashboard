80022253a4d37b01ecd4e2ed51581b3a3a73854e
Sarah <sacriscione@gmail.com>
2024-10-20 20:47:39 -0400

    Updates search and login

--- nextjs-dashboard/src/app/ui/search.jsx
+++ nextjs-dashboard/src/app/ui/search.jsx
@@ -3,7 +3,7 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search() {
+export default function Search({ placeholder }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
